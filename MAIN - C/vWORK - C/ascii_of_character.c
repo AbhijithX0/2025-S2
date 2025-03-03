0753daaabd67931eb5107d9e6cd70b51433beeed
Abhijith <abhijithbinu2024@gmail.com>
2025-03-03 21:09:57 +0530

    ascii

---
+++ MAIN - C/vWORK - C/ascii_of_character.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    printf("The ASCII value of the entered character is %d", ch);
+}
+
+//Github : AbhijithX0
