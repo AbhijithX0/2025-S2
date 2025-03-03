687486684eb8f007cb0d9a924eccd11c3fbdae2f
Abhijith <abhijithbinu2024@gmail.com>
2025-03-03 21:07:26 +0530

    even or odd

---
+++ MAIN - C/vWORK - C/even_or_odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num%2==0){
+        printf("The entered number is even");
+    }
+    else{
+        printf("The entered number is odd");
+    }
+}
+
+//Github : AbhijithX0
